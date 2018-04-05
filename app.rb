375d277a5f6e4643b6e66fd8be50760b6069cc1d
Sam. O <samrey2018@gmail.com>
2018-04-05 03:59:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -23,7 +23,8 @@ class App < Sinatra::Base
 
   get '/multiply/:num1/:num2' do
     binding.pry
-  @goodbye = params[:num1, ]
+  @spot_1 = params[:num1]
+  @spot_2 = params[:num2]
     "(num1 num2)."
   end
 
